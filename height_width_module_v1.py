5c76751da7c28000f048bc5d017c667d1bcf0378
Custom Solutions <74469642+stainezy34@users.noreply.github.com>
2023-07-13 19:05:26 +0100

    Update height_width_module_v1.py

--- height_width_module_v1.py
+++ height_width_module_v1.py
@@ -1,13 +1,14 @@
-from height_width_module_v1 import get_height, get_roof_type
 from unit_configuration_module import calculate_total_units, determine_vehicle_type, get_unit_configuration
+from product_description_manager import save_product_description
+from height_roof_module import get_height
 
 def generate_product_description(sku):
     height = get_height(sku)
     roof_type = get_roof_type(height)
-    product_description = 'SET-{}\n'.format(sku[len("SET-"):])
-    product_description += 'Height: {}mm\n\n'.format(height)
+    product_description = f'SET-{sku[len("SET-"):]}\n'
+    product_description += f'Height: {height}mm\n\n'
     product_description += 'Product Description:\n'
-    product_description += 'Unit Height: {}mm ({})\n'.format(height, roof_type)
+    product_description += f'Unit Height: {height}mm ({roof_type})\n'
 
     width_unit = int(sku.split('-')[2])  # Extract the width unit from SKU
     total_install = int(sku.split('-')[3])  # Extract the total install width from SKU
@@ -18,23 +19,38 @@ def generate_product_description(sku):
     if unit_configuration:
         product_description += 'Unit Configuration:\n'
         for i, unit_width in enumerate(unit_configuration):
-            product_description += '{} {}mm Standard Systainer\n'.format(i + 1, unit_width)
+            product_description += f'{i+1}\N{COMBINING ENCLOSING KEYCAP} {unit_width}mm Standard Systainer\n'
     else:
         product_description += 'No exact unit configuration found.'
 
     vehicle_type = determine_vehicle_type(total_install)
-    product_description += 'Suits {} (total install length: {}mm)'.format(vehicle_type, total_install)
+    product_description += f'\nSuits {vehicle_type} (total install length: {total_install}mm)'
 
     total_widths = sum(unit_configuration) if unit_configuration else 0
     if total_widths > total_install:
         units_over = total_widths - total_install
-        product_description += '\nUnits Configured Over Total Length: {}mm'.format(units_over)
+        product_description += f"\nUnits Configured Over Total Length: {units_over}mm"
     elif total_widths < total_install:
         units_below = total_install - total_widths
-        product_description += '\nUnits Configured Below Total Length: {}mm'.format(units_below)
+        product_description += f"\nUnits Configured Below Total Length: {units_below}mm"
 
     difference = abs(total_widths - total_install)
-    product_description += '\nDifference between Total Units Widths and Install Length: {}mm'.format(difference)
+    product_description += f"\nDifference between Total Units Widths and Install Length: {difference}mm"
+
+    # Save the product description
+    save_product_description(sku, product_description)
 
     return product_description
 
+
+def input_set_number():
+    while True:
+        sku = input("Enter the SET number or SKU (or 'q' to quit): ")
+        if sku.lower() == 'q':
+            break
+        description = generate_product_description(sku)
+        print(description)
+
+
+# Call the input function
+input_set_number()
